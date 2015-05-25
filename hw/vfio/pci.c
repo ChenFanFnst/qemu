84428431ec27624eff715abe3b56eadbfa408206
Chen Fan <chen.fan.fnst@cn.fujitsu.com>
2015-05-25 09:12:01 +0800

    vfio: add 'aer' property to expose aercap
    add 'aer' property to let user able to decide whether expose
    the aer capability. by default we should disable aer feature,
    because it needs configuration restrictions.
    
    Signed-off-by: Chen Fan <chen.fan.fnst@cn.fujitsu.com>

--- hw/vfio/pci.c
+++ hw/vfio/pci.c
@@ -3128,6 +3128,8 @@ static Property vfio_pci_dev_properties[] = {
                        sub_vendor_id, PCI_ANY_ID),
     DEFINE_PROP_UINT32("x-pci-sub-device-id", VFIOPCIDevice,
                        sub_device_id, PCI_ANY_ID),
+    DEFINE_PROP_BIT("aer", VFIOPCIDevice, features,
+                    VFIO_FEATURE_ENABLE_AER_BIT, false),
     /*
      * TODO - support passed fds... is this necessary?
      * DEFINE_PROP_STRING("vfiofd", VFIOPCIDevice, vfiofd_name),
